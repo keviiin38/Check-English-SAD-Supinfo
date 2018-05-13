9621bd71e3990e36bb2ef9ca59461b5f964dc675
Kevin <34242560+keviiin38@users.noreply.github.com>
2018-05-13 21:11:43 +0200

    Added the main file and the INSTALL file

---
+++ INSTALL.sh
@@ -0,0 +1,146 @@
+#!/bin/bash
+
+read -p "Which browser would you use ? (Firefox/Chromium/Chrome) : " BROWSER
+case "$BROWSER" in
+    [fF][iI][rR][eE][fF][oO][xX]) BROWSER="firefox"; echo ;;
+    [cC][hH][rR][oO][mM][eE]) BROWSER="chrome"; echo ;;
+    [cC][hH][rR][oO][mM][iI][uU][mM]) BROWSER="chromium"; echo ;;
+    *) exit 1;;
+esac
+
+if ( ! dpkg -s python-pip &> /dev/null ); then
+    echo "Please install python-pip"
+    read -p "Would you like to run the install now (You'll be prompted for the root password) ? (Yes/No) : " answer
+    case "$answer" in
+        [yY] | [yY][eE][sS]) su -c "apt-get install python-pip -y && echo -e \"\npython-pip installed !\n\"" root;;
+        [nN] | [nN][oO]) exit 1 ;;
+        *) exit 1;;
+    esac
+    unset answer
+fi
+
+if ( ! python3 -c "import selenium" &> /dev/null); then
+    echo "Please install Python module selenium"
+    read -p "Would you like to run the install now ? (Yes/No) : " answer
+    case "$answer" in
+        [yY] | [yY][eE][sS]) pip install --user selenium && echo -e "\nselenium installed !\n" ;;
+        [nN] | [nN][oO]) exit 1 ;;
+        *) exit 1;;
+    esac
+    unset answer
+fi
+
+case "$BROWSER" in
+    firefox)
+        if ! [ -e /usr/bin/firefox -o -e /usr/local/bin/firefox ]; then
+            echo "Please install firefox"
+            read -p "Would you like to run the install now (You'll be prompted for the root password) ? (Yes/No) : " answer
+            case "$answer" in
+                [yY] | [yY][eE][sS]) su -c "apt-get install firefox -y && echo -e \"\nfirefox installed !\n\"" root ;;
+                [nN] | [nN][oO]) exit 1 ;;
+                *) exit 1;;
+            esac
+            unset answer
+        fi ;;
+    chromium)
+        if ! [ -e /usr/bin/chromium-browser -o -e /usr/local/bin/chromium-browser ]; then
+            echo "Please install chromium-browser"
+            read -p "Would you like to run the install now (You'll be prompted for the root password) ? (Yes/No) : " answer
+                case "$answer" in
+                [yY] | [yY][eE][sS]) su -c "apt-get install chromium-browser -y && echo -e \"\nchromium-browser installed !\n\"" root ;;
+                [nN] | [nN][oO]) exit 1 ;;
+                *) exit 1;;
+            esac
+            unset answer
+        fi ;;
+    chrome)
+        if ! [ -e /usr/bin/google-chrome-stable -o -e /usr/local/bin/google-chrome-stable ]; then
+            echo "Please install google-chrome-stable"
+            read -p "Would you like to run the install now (You'll be prompted for the root password) ? (Yes/No) : " answer
+                case "$answer" in
+                [yY] | [yY][eE][sS])
+                    cd /tmp
+                    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+                    echo
+                    su -c "apt-get install /tmp/google-chrome-stable_current_amd64.deb -y &> /dev/null" root
+                    rm google-chrome-stable_current_amd64.deb
+                    echo -e "\ngoogle-chrome-stable installed !\n"
+                    cd - &> /dev/null ;;
+                [nN] | [nN][oO]) exit 1 ;;
+                *) exit 1;;
+            esac
+            unset answer
+    fi ;;
+    *) exit 1 ;;
+esac
+
+case "$BROWSER" in
+    firefox)
+        if ! [ -e /usr/bin/geckodriver -o -e /usr/local/bin/geckodriver ]; then
+            echo "Please install geckodriver"
+            read -p "Would you like to run the install now (You'll be prompted for the root password) ? (Yes/No) : " answer
+            case "$answer" in
+                [yY] | [yY][eE][sS])
+                    cd /tmp
+                    wget https://github.com/mozilla/geckodriver/releases/download/v0.20.1/geckodriver-v0.20.1-linux64.tar.gz
+                    tar -zxvf geckodriver-v0.20.1-linux64.tar.gz &> /dev/null
+                    echo
+                    su -c "mv geckodriver /usr/bin/geckodriver" root
+                    rm geckodriver-v0.20.1-linux64.tar.gz
+                    echo -e "\ngeckodriver installed !\n"
+                    cd - &> /dev/null ;;
+                [nN] | [nN][oO]) exit 1 ;;
+                *) exit 1 ;;
+            esac
+        fi ;;
+    chrome | chromium)
+        if ! [ -e /usr/bin/chromedriver -o -e /usr/local/bin/chromedriver ]; then
+            echo "Please install chromedriver"
+            read -p "Would you like to run the install now (You'll be prompted for the root password) ? (Yes/No) : " answer
+            case "$answer" in
+                [yY] | [yY][eE][sS])
+                    cd /tmp
+                    if [ -e /tmp/chromedriver ]; then
+                        rm chromedriver
+                    fi
+                    if [ -e /tmp/chromedriver_linux64.zip ]; then
+                        rm chromedriver_linux64.zip
+                    fi
+                    echo
+                    wget https://chromedriver.storage.googleapis.com/2.38/chromedriver_linux64.zip
+                    echo
+                    unzip chromedriver_linux64.zip &> /dev/null
+                    su -c "mv chromedriver /usr/bin/chromedriver" root
+                    if [ $? -eq 1 ]; then
+                        rm chromedriver_linux64.zip
+                        exit 1
+                    fi
+                    rm chromedriver_linux64.zip
+                    echo -e "\nchromedriver installed !\n"
+                    cd - &> /dev/null ;;
+                [nN] | [nN][oO]) exit 1 ;;
+                *) exit 1;;
+            esac
+        fi ;;
+    *) exit 1 ;;
+esac
+
+
+echo -e "All requirements installed !\n"
+
+read -p "ID Booster : " IDBOOSTER
+read -s -p "Campus Booster password : " SUPINFO_PASSWORD
+read -p "GMail address : " GMAIL_ADDRESS
+read -s -p "GMail password : " GMAIL_PASSWORD
+read -p "Receiver address : " RECEIVER_ADDRESS
+
+sed -i "s/IDBOOSTER =.*/IDBOOSTER = \"$IDBOOSTER\"/" check_english_sad_supinfo.py
+sed -i "s/SUPINFO_PASSWORD =.*/SUPINFO_PASSWORD = \"$SUPINFO_PASSWORD\"/" check_english_sad_supinfo.py
+sed -i "s/GMAIL_ADDRESS =.*/GMAIL_ADDRESS = \"$GMAIL_ADDRESS\"/" check_english_sad_supinfo.py
+sed -i "s/GMAIL_PASSWORD =.*/GMAIL_PASSWORD = \"$GMAIL_PASSWORD\"/" check_english_sad_supinfo.py
+sed -i "s/RECEIVER_ADDRESS =.*/RECEIVER_ADDRESS = \"$RECEIVER_ADDRESS\"/" check_english_sad_supinfo.py
+sed -i "s/BROWSER = \".*/BROWSER = \"$BROWSER\"/" check_english_sad_supinfo.py
+
+unset IDBOOSTER SUPINFO_PASSWORD GMAIL_ADDRESS GMAIL_PASSWORD RECEIVER_ADDRESS BROWSER
+
+echo -e "\nInstall finished !"
---
+++ check_english_sad_supinfo.py
@@ -0,0 +1,147 @@
+#!/usr/bin/python3
+
+import smtplib
+import time
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+from selenium import webdriver
+
+IDBOOSTER = ""
+SUPINFO_PASSWORD = ""
+GMAIL_ADDRESS = ""
+GMAIL_PASSWORD = ""
+RECEIVER_ADDRESS = ""
+BROWSER = ""
+
+if "" in (IDBOOSTER, SUPINFO_PASSWORD, GMAIL_ADDRESS, GMAIL_PASSWORD, RECEIVER_ADDRESS, BROWSER):
+    print("Please run INSTALL.sh first !")
+    exit(1)
+
+
+def check_available_slot(inner_html):
+    if "<span style=\"color: green;\">" in inner_html:
+        return True
+    else:
+        return False
+
+
+def get_date(inner_html):
+    if "<span style=\"color: green;\">" not in inner_html:
+        return False
+
+    # Get the Year
+    i = inner_html.index("2018</strong>")
+    i_end = i + 4
+
+    # Get the month
+    while inner_html[i] != ">":
+        i -= 1
+
+    # Get the month and year
+    date = str(inner_html[i + 1:i_end])
+
+    # Get the time
+    i = inner_html.index("<span style=\"color: green;\">")
+    slot_time = inner_html[i + 28: i + 30] + "h to " + inner_html[i + 37: i + 39] + "h"
+
+    # Get the day number
+    i = inner_html.index("<span style=\"color: green;\">")
+
+    while inner_html[i:i + 18] != "<span class=\"day\">":
+        i += 1
+
+    # Add some text
+    date += " from " + slot_time
+
+    if inner_html[i + 19] == "<":
+        return str(inner_html[i + 18] + " " + date)
+    else:
+        return str(inner_html[i + 18:i + 20] + " " + date)
+
+
+def send_mail(place_time):
+    msg = MIMEMultipart()
+    msg['From'] = GMAIL_ADDRESS
+    msg['To'] = RECEIVER_ADDRESS
+    msg['Subject'] = "[English - Check] " + str(len(place_time)) + " slot(s) available !"
+
+    body = "They are " + str(len(place_time)) + " slot(s) available on http://english.sad.supinfo.com/\n\n"
+
+    for slot_time in place_time:
+        body += "The " + slot_time + "\n\n"
+
+    msg.attach(MIMEText(body))
+
+    mail_server = smtplib.SMTP('smtp.gmail.com', 587)
+    mail_server.starttls()
+    mail_server.login(GMAIL_ADDRESS, GMAIL_PASSWORD)
+    mail_server.sendmail(GMAIL_ADDRESS, RECEIVER_ADDRESS, msg.as_string())
+    mail_server.quit()
+
+
+def get_html_site():
+    if BROWSER == "firefox":
+        options = webdriver.FirefoxOptions()
+        options.add_argument("--headless")
+        browser = webdriver.Firefox(firefox_options=options)
+    elif BROWSER in ("chrome", "chromium"):
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless")
+        browser = webdriver.Chrome(chrome_options=options)
+
+    # Go to the English website
+    browser.get("http://english.sad.supinfo.com")
+
+    time.sleep(2)
+
+    # Find and input the IDBOOSTER
+    id_booster = browser.find_element_by_name("boosterId")
+    id_booster.send_keys(IDBOOSTER)
+
+    # Log in to the website
+    button = browser.find_element_by_xpath("//input[@value='Connexion']")
+    button.click()
+
+    time.sleep(2)
+
+    # Log in to the SSO
+    password = browser.find_element_by_id("Password")
+    password.send_keys(SUPINFO_PASSWORD)
+
+    button = browser.find_element_by_id("LoginButton")
+    button.click()
+
+    time.sleep(2)
+
+    # Go to the calendar tab
+    browser.get("http://english.sad.supinfo.com/student/calendar")
+
+    time.sleep(2)
+
+    return browser
+
+
+def main():
+    browser = get_html_site()
+
+    slot_time = []
+
+    # Check for the current month and the 4 next ones
+    for i in range(5):
+        if i != 0:
+            next_month_button = browser.find_element_by_xpath("//*[contains(text(),'>')]")
+            next_month_button.click()
+
+        inner_html = browser.execute_script("return document.body.innerHTML")
+
+        if check_available_slot(inner_html):
+            slot_time.append(get_date(inner_html))
+
+    browser.quit()
+
+    if not slot_time:
+        send_mail(slot_time)
+
+
+main()
